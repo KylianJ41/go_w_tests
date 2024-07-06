bb4fb43868a0c621ccbde6d01c2fda05682035c5
Kikito <kylian.jozefiak@etu.unistra.fr>
2024-07-06 16:21:39 +0200

    Methods, structs and interfaces DONE

--- structsMethodInterfaces/shapes.go
+++ structsMethodInterfaces/shapes.go
@@ -21,6 +21,16 @@ func (c Circle) Area() float64 {
 	return c.Radius * c.Radius * 3.141592653589793
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	//return 0.5 * t.Base * t.Height
+	return 0
+}
+
 func Perimeter(rect Rectangle) float64 {
 	return 2 * (rect.Width + rect.Height)
 }
