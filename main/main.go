4bbf46d9770417b49d52ef4489ec520ba0454004
Kikito <kylian.jozefiak@etu.unistra.fr>
2024-07-12 22:06:08 +0200

    mocks finished, roman done

--- main/main.go
+++ main/main.go
@@ -22,10 +22,43 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+const write = "write"
+const sleep = "sleep"
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
@@ -36,7 +69,22 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 	fmt.Fprint(out, finalWord)
 }
 
+/*
+VEERSION TO TEST IF IT PASS EVEN IF THE SLEEP/PRINT IS OUT OF ORDER
+
+	func Countdown(out io.Writer, sleeper Sleeper) {
+		for i := countdownStart; i > 0; i-- {
+			sleeper.Sleep()
+		}
+
+		for i := countdownStart; i > 0; i-- {
+			fmt.Fprintln(out, i)
+		}
+
+		fmt.Fprint(out, finalWord)
+	}
+*/
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
